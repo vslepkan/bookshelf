95d6b90c3330848767b51ca6058d0aa041116453
Vladyslav Slepkan <vslepkan@gmail.com>
2020-06-16 15:52:15 +0300

    chore: add initial files

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className='p-4 text-xl font-bold text-center'>[Book | Movie | Game] shelf</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
